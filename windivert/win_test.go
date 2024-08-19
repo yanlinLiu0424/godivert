99ed9952bf26efe81ac9206760c3b2a17b3608fb
yanlin <yanlinliu@atop.com.tw>
2024-08-19 16:19:33 +0800

    fix bug of ipv6

--- windivert/win_test.go
+++ windivert/win_test.go
@@ -17,3 +17,26 @@ func TestIPv6(t *testing.T) {
 	log.Print(addr.DstIP())
 	log.Print(addr.String())
 }
+func TestIPv61(t *testing.T) {
+	data := []byte{
+		0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
+	}
+	addr := Packet{Raw: data}
+	log.Print(addr.SrcIP())
+	log.Print(addr.DstIP())
+	log.Print(addr.String())
+}
+
+func TestT1n(t *testing.T) {
+	v := "6000000000240001fe80000000000000bd3b48b576234f95ff0200000000000000000000000000163a00050200000100"
+	data, err := hex.DecodeString(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	addr := Packet{Raw: data}
+	log.Print(addr.SrcIP())
+	log.Print(addr.DstIP())
+	log.Print(addr.String())
+}
